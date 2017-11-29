72f996851f48d7b330d00bd0de65ede1632d37d8
ganchuanpu <429533813@qq.com>
2017-11-29 20:01:05 +0800

    AsyncTask内部类内存泄漏bug

--- app/src/main/java/com/goldenasia/lottery/fragment/ResultsMmcFragment.java
+++ app/src/main/java/com/goldenasia/lottery/fragment/ResultsMmcFragment.java
@@ -1,5 +1,6 @@
 package com.goldenasia.lottery.fragment;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -14,6 +15,7 @@ import com.goldenasia.lottery.db.MmcWinHistory;
 import com.goldenasia.lottery.db.MmcWinHistoryDao;
 import com.goldenasia.lottery.view.adapter.HistoryCodeMmcAdapter;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 import butterknife.BindView;
@@ -28,8 +30,7 @@ public class ResultsMmcFragment extends BaseFragment {
 
     @BindView(R.id.history_lottery_listviewcode)
     ListView listView;
-    private HistoryCodeMmcAdapter adapter;
-    private MmcWinHistoryDao mMmcWinHistoryDao;
+    private static HistoryCodeMmcAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -39,30 +40,48 @@ public class ResultsMmcFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mMmcWinHistoryDao = new MmcWinHistoryDao(getActivity());
         adapter = new HistoryCodeMmcAdapter();
         listView.setAdapter(adapter);
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
-        new LoadDataAsyncTask().execute();
+        new LoadDataAsyncTask(getActivity()).execute();
     }
 
+    private static class LoadDataAsyncTask extends AsyncTask<Void, Void, List<MmcWinHistory>> {
+        private final WeakReference<Context> mContext;
 
-    class LoadDataAsyncTask extends AsyncTask<Void, Void, List<MmcWinHistory>> {
+        public LoadDataAsyncTask(Context context) {
+            mContext = new WeakReference<Context>(context);
+        }
 
         @Override
         protected List<MmcWinHistory> doInBackground(Void... params) {
+
+            Context context = mContext.get();
+
+            if(context==null){
+                return  null;
+            }
+            MmcWinHistoryDao mMmcWinHistoryDao = new MmcWinHistoryDao(context);
+
             return mMmcWinHistoryDao.getAllMmcWinHistory();
         }
 
         @Override
         protected void onPostExecute(List<MmcWinHistory> mmcWinHistories) {
-            adapter.setCodeList(mmcWinHistories);
-            adapter.notifyDataSetChanged();
+            if(mmcWinHistories!=null&&adapter!=null) {
+                adapter.setCodeList(mmcWinHistories);
+                adapter.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
         }
     }
 }
