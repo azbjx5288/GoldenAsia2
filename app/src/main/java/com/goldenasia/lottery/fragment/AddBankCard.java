27565b0a15777186d51144407076c2ceb49692b7
ganchuanpu <429533813@qq.com>
2018-03-07 19:54:09 +0800

    数据校验修改

--- app/src/main/java/com/goldenasia/lottery/fragment/AddBankCard.java
+++ app/src/main/java/com/goldenasia/lottery/fragment/AddBankCard.java
@@ -209,8 +209,8 @@ public class AddBankCard extends BaseFragment {
             return false;
         }
 
-        //长度6~15，不能只有数字也不能只有字母
-        if (securityPassword.getText().toString().matches("^[a-zA-Z0-9]{6,15}$")) {
+        //长度6~20，不能只有数字也不能只有字母
+        if (securityPassword.getText().toString().matches("^[a-zA-Z0-9]{6,20}$")) {
             Toast.makeText(getActivity(), "资金密码长度为6~15，不能只有数字也不能只有字母", Toast.LENGTH_SHORT).show();
             return false;
         }
