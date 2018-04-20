9e151b9e5c2cbccfc8cac3538e177ca123f2c37a
ganchuanpu <429533813@qq.com>
2018-04-20 15:43:17 +0800

    山东快乐扑克开奖显示修改

--- app/src/main/java/com/goldenasia/lottery/view/adapter/HistoryCodeAdapter.java
+++ app/src/main/java/com/goldenasia/lottery/view/adapter/HistoryCodeAdapter.java
@@ -1,13 +1,17 @@
 package com.goldenasia.lottery.view.adapter;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.goldenasia.lottery.R;
 import com.goldenasia.lottery.data.IssueEntity;
+import com.goldenasia.lottery.util.UiUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +25,13 @@ import butterknife.ButterKnife;
 public class HistoryCodeAdapter extends BaseAdapter {
 
     private List codeList;
+    private int mLotteryId;
+    private Context mContext;
 
-    public HistoryCodeAdapter(List codeList){
+    public HistoryCodeAdapter(List codeList,int lotteryId,Context context){
         this.codeList=codeList;
+        mLotteryId=lotteryId;
+        mContext=context;
     }
 
     @Override
@@ -55,19 +63,81 @@ public class HistoryCodeAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        ViewHolder holder;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_history_result_item, parent, false);
-            holder = new ViewHolder(convertView);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
+        if(mLotteryId==14) {//山东快乐扑克
+            ViewHolderShandongKuailePuke holder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_history_result_item_shandongkuailepuke, parent, false);
+                holder = new ViewHolderShandongKuailePuke(convertView);
+            } else {
+                holder = (ViewHolderShandongKuailePuke) convertView.getTag();
+            }
+            IssueEntity historyCode=(IssueEntity)codeList.get(position);
+            holder.issue.setText(historyCode.getIssue());
+
+            //开奖号码
+            String codeOpen=historyCode.getCode();
+            holder.textSdlklpu01.setText(codeOpen.charAt(0)+"");
+            holder.textSdlklpu02.setText(codeOpen.charAt(3)+"");
+            holder.textSdlklpu03.setText(codeOpen.charAt(6)+"");
+
+            //图片
+            holder.imageSdlklpu01.setImageDrawable(letterToDrawable(codeOpen.charAt(1)+""));
+            holder.imageSdlklpu02.setImageDrawable(letterToDrawable(codeOpen.charAt(4)+""));
+            holder.imageSdlklpu03.setImageDrawable(letterToDrawable(codeOpen.charAt(7)+""));
+
+        }else{
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_history_result_item, parent, false);
+                holder = new ViewHolder(convertView);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            IssueEntity historyCode=(IssueEntity)codeList.get(position);
+            holder.issue.setText(historyCode.getIssue());
+            holder.code.setText(historyCode.getCode());
         }
-        IssueEntity historyCode=(IssueEntity)codeList.get(position);
-        holder.issue.setText(historyCode.getIssue());
-        holder.code.setText(historyCode.getCode());
 
         return convertView;
     }
+
+    //根据字母判定花色  花色♠s,♥h ♣c ♦d(一律小写)
+    private Drawable letterToDrawable(String letter){
+        if ("s".equals(letter)){
+            return UiUtils.getDrawable(mContext,R.drawable.ht_icon);
+        }else if("h".equals(letter)){
+            return UiUtils.getDrawable(mContext,R.drawable.hongt_icon);
+        }else if("c".equals(letter)){
+            return UiUtils.getDrawable(mContext,R.drawable.mh_icon);
+        }else if("d".equals(letter)){
+            return UiUtils.getDrawable(mContext,R.drawable.fk_icon);
+        }else{
+            return null;
+        }
+    }
+
+    static class ViewHolderShandongKuailePuke {
+        @BindView(R.id.historycode_issue)
+        TextView issue;
+        @BindView(R.id.image_sdlklpu01)
+        ImageView imageSdlklpu01;
+        @BindView(R.id.image_sdlklpu02)
+        ImageView imageSdlklpu02;
+        @BindView(R.id.image_sdlklpu03)
+        ImageView imageSdlklpu03;
+        @BindView(R.id.text_sdlklpu01)
+        TextView textSdlklpu01;
+        @BindView(R.id.text_sdlklpu02)
+        TextView textSdlklpu02;
+        @BindView(R.id.text_sdlklpu03)
+        TextView textSdlklpu03;
+
+        public ViewHolderShandongKuailePuke(View view) {
+            ButterKnife.bind(this, view);
+            view.setTag(this);
+        }
+    }
+
     static class ViewHolder {
         @BindView(R.id.historycode_issue)
         TextView issue;
